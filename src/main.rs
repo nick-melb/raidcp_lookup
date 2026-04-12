67c5246adc66ea65b9b194202ca8f68e173a9bf7
Nicholas <nicholasfield@gmail.com>
2026-04-12 15:15:31 +1000

    added data.csv

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,42 @@
-use std::io;
+use serde::Deserialize;
+//use std::error::Error;
+//use std::fs::File;
+
+struct Pokemon {
+    number: &'static str,
+    name: &'static str,
+    hundo_cp: &'static str,
+    hundo_wb_cp: &'static str,
+    weather0: &'static str,
+    weather1: &'static str,
+    raid_tier: &'static str,
+    type0: &'static str,
+    type1: &'static str,
+    shiny_enable: bool,
+}
 
 fn main() {
-    println!("Welcome!");
-    
-    let mon: (u32, str&, u32, u32, str&, str&, u32, str&, str&, bool) = (26, "Raichu", 1247, 1558, "Rainy", "", 3, "Electric", "", 1);
+    //pull in csv at compile time
+    let data = include_str!("data.csv");
 
-    println!({mon.0}, {mon.1}, {mon.2}, {mon.3}, {mon.4}, {mon.5}, {mon.6}, {mon.7}, {mon.8}, {mon.9});
+    //csv reader
+    let mut rdr = csv::Reader::from_reader(data.as_bytes());
+
+    // sort data from data.csv
+    for result in rdr.deserialize::<Pokemon>() {
+        match result {
+        Ok(p) => println! ("Found {} (Shiny: {})", p.name, p.shiny_enable),
+        Err(e) => eprintln!("Error: {}", e),
+        }
+    }
 }
+// old data below
+//fn main() {
+//    let mon_list = vec!
+//        Pokemon {
+//    println!("Welcome!");
+    
+//    let mon: (u32, str&, u32, u32, str&, str&, u32, str&, str&, bool) = (26, "Raichu", 1247, 1558, "Rainy", "", 3, "Electric", "", 1);
+
+  //  println!({mon.0}, {mon.1}, {mon.2}, {mon.3}, {mon.4}, {mon.5}, {mon.6}, {mon.7}, {mon.8}, {mon.9});
+//}
